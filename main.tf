7f9fb434cda3b60e39f1789f58b82bbb189ea829
Mark Nizzle <marknizzle84@gmail.com>
2023-01-20 22:04:27 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+    }
+  }
+}
+
+provider "oci" {
+  region              = "us-ashburn-1"
+  auth                = "SecurityToken"
+  config_file_profile = "learn-terraform"
+}
+
+resource "oci_core_vcn" "internal" {
+  dns_label      = "internal"
+  cidr_block     = "172.16.0.0/20"
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaay7axxquulavcanae6lpym5ruwwmgax3sgb6slmpzh4d5lfkwtepa"
+  display_name   = "My internal VCN"
+}
